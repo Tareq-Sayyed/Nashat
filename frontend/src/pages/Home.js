956ff4a463f0fd58788bdb9bff082f4d3752f70b
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-05-30 06:09:50 +0300

    Added the WorkoutDetails component which gets the data fed to it as props from the home component after it is fetched from the database using the fetch api. { added the proxy element in the package.json file }

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+    // It is null because if the res.ok is data itll be set to true else itll be false.
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const res = await fetch('/api/workouts');
+            const data = await res.json();
+            console.log('Data:', data); // Add this console log
+            if (res.ok) {
+                setWorkouts(data.workouts);
+                console.log('Updated Workouts:', workouts); // Add this console log
+            }
+            
+        };
+    
+        fetchWorkouts();
+    }, []);
+    
+    console.log('Workouts:', workouts); // Add this console log
+    
+
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={ workout._id } workout={ workout } />
+                ))}
+            </div>
         </div>
+
      );
 }
  
