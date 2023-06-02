34a7f854424671685ab68cf68d7bdfcf46ff55ad
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-06-02 08:12:32 +0300

    Added the logo to the favicon & navbar also edited the CSS of the page to fix some stuff

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import Logo from "./1aac83.png"
 const Navbar = () => {
     return ( 
         <header>
             <div className="container">
-                <Link to = "/"><h1>NASHAT</h1></Link>
+                <Link className = "LINKS" to = "/"><img src={ Logo } style={{ maxWidth: '50px', maxHeight: '50px'}} alt = ""/><h1>NASHAT</h1></Link>
             </div>
         </header>
      );
