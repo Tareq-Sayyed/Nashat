ed3b2aa7afb0788fd2f2054e8c2b598771c9d3ed
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-06-04 11:18:01 +0300

    Modified all the files that had to do with the sign-up, as there was some CORS error that I could not pinpoint. Sgining up works now but the error messages under the form do not show up

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,35 +1,29 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
-    switch (action.type) {
-        case "LOGIN":
-            localStorage.setItem("user", JSON.stringify(action.payload.user));
-            return {
-                ...state,
-                user: action.payload.user
-            };
-        case "LOGOUT":
-            localStorage.clear();
-            return {
-                ...state,
-                user: null
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'LOGIN':
+      return { user: action.payload }
+    case 'LOGOUT':
+      return { user: null }
+    default:
+      return state
+  }
 }
 
-
 export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null
-    });
-    console.log('AuthContext state: ', state);
-    return (
-        <AuthContext.Provider value={{ ...state, dispatch }}>
-            { children }
-        </AuthContext.Provider>
-    );
-};
+  const [state, dispatch] = useReducer(authReducer, { 
+    user: null
+  })
+
+  console.log('AuthContext state:', state)
+  
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      { children }
+    </AuthContext.Provider>
+  )
+
+}
