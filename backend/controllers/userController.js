9ff9cec2a5f7d5c2aa0911f179d93565fea2aa11
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2023-06-04 09:50:33 +0300

    Installed the JWT package and made a token that is made when a user signs up to NASHAT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,12 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// Creating a token.
+const createToken = (_id) => {
+    // 1st argument: payload. 2nd argument: secret. 3rd argument: options.
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'});
+}
+
 
 // Login user.
 const loginUser = async (req, res) => {
@@ -10,7 +18,11 @@ const signUpUser = async (req, res) => {
     const {email, password} = req.body;
     try{
         const user = await User.signup(email, password);
-        res.status(201).json({email, user});
+        // Create token.
+        const token = createToken(user._id);
+        // Set cookie.
+        // res.cookie('jwt', token, {httpOnly: true, maxAge: 3 * 24 * 60 * 60 * 1000});
+        res.status(201).json({email, token});
     } catch(error){
         res.status(400).json({msg: error.message});
     }
